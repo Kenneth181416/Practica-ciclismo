22295dadef52a74b046b4c2c5cb9fcce177ea13e
kenneth ibarra <kennethivelasco1118@gmail.com>
2025-04-08 13:59:24 -0500

    Se actualizan las clases nuevamente

--- src/parctica/ciclismo/Contrarelojista.java
+++ src/parctica/ciclismo/Contrarelojista.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package parctica.ciclismo;
 
-public class Contrarrelojista extends Ciclista {
+public class Contrarelojista extends Ciclista {
     private double velocidadMaxima;
 
-    public Contrarrelojista(int id, String nombre, double velocidadMaxima) {
+    public Contrarelojista(int id, String nombre, double velocidadMaxima) {
         super(id, nombre);
         this.velocidadMaxima = velocidadMaxima;
     }
